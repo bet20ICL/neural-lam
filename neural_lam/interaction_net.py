fix border masking

neural_lam/interaction_net.py
@@ -51,15 +51,8 @@ class InteractionNet(pyg.nn.MessagePassing):
         if hidden_dim is None:
             # Default to input dim if not explicitly given
             hidden_dim = input_dim
-
-        # Make both sender and receiver indices of edge_index start at 0
-        edge_index = edge_index - edge_index.min(dim=1, keepdim=True)[0]
-        # Store number of receiver nodes according to edge_index
-        self.num_rec = edge_index[1].max() + 1
-        edge_index[0] = (
-            edge_index[0] + self.num_rec
-        )  # Make sender indices after rec
-        self.register_buffer("edge_index", edge_index, persistent=False)
+            
+        self.set_edge_index(edge_index)
 
         # Create MLPs
         edge_mlp_recipe = [3 * input_dim] + [hidden_dim] * (hidden_layers + 1)
@@ -82,6 +75,19 @@ class InteractionNet(pyg.nn.MessagePassing):
             )
 
         self.update_edges = update_edges
+        
+    def set_edge_index(self, edge_index):
+        """
+        Set new edge index for the InteractionNet
+        """        
+        # Make both sender and receiver indices of edge_index start at 0
+        edge_index = edge_index - edge_index.min(dim=1, keepdim=True)[0]
+        # Store number of receiver nodes according to edge_index
+        self.num_rec = edge_index[1].max() + 1
+        edge_index[0] = (
+            edge_index[0] + self.num_rec
+        )  # Make sender indices after rec
+        self.register_buffer("edge_index", edge_index, persistent=False)
 
     def forward(self, send_rep, rec_rep, edge_rep):
         """

setup_multiresolution_data.py
@@ -55,23 +55,39 @@ def multi_res_edges(fine_graph, full_coarse_graph, coarse_graph):
     coarse2fine_edge_set = {tuple(e) for e in coarse2fine_edge_index.T}
     return coarse2fine_edge_index, coarse2fine_edge_set
     
+def create_border_mask():
+    full_dataset = "era5_uk_big"
+    local_dataset = "era5_uk_small"
+    full_grid = np.load(f"./data/{full_dataset}/static/nwp_xy.npy")
+    local_grid = np.load(f"./data/{local_dataset}/static/nwp_xy.npy")
+    
+    print("Full dataset shape:", full_grid.shape)
+    print("Local dataset shape:", local_grid.shape)
+    
+    full_grid_2d = full_grid.reshape(2, -1).T
+    local_grid_2d = local_grid.reshape(2, -1).T
+    mask = np.isin(full_grid_2d, local_grid_2d).all(axis=1)
+    mask = 1.0 - mask.reshape(full_grid.shape[1:]).astype(int)
+    # np.save(f"./data/{full_dataset}/static/border_mask.npy", mask)
+    np.save(f"./data/era5_uk_small/static/border_mask.npy", mask)
+    
 def uk_hierarchy():
     # Download the datsets
     small_dataset = "era5_uk_small"
     big_dataset = "era5_uk_big"
     coarse_big_dataset = f"{big_dataset}_coarse" 
     
-    # subset = era5_data_proc.uk_subset
+    subset = era5_data_proc.uk_subset
     # era5_data_proc.save_dataset_samples(small_dataset, subset=subset)
-    # era5_data_proc.create_xy(small_dataset, subset=subset)
+    era5_data_proc.create_xy(small_dataset, subset=subset)
     
     # subset = era5_data_proc.uk_big_subset
     # # era5_data_proc.save_dataset_samples(big_dataset, subset=subset) # don't need to save the full samples
     # era5_data_proc.create_xy(big_dataset, subset=subset) # just need this grid for graph generation
     
-    subset = era5_data_proc.uk_big_subset
-    era5_data_proc.save_dataset_samples(coarse_big_dataset, subset=subset, coarsen_fn=subsample())
-    era5_data_proc.create_xy(coarse_big_dataset, subset=subset, coarsen_fn=subsample())
+    # subset = era5_data_proc.uk_big_subset
+    # era5_data_proc.save_dataset_samples(coarse_big_dataset, subset=subset, coarsen_fn=subsample())
+    # era5_data_proc.create_xy(coarse_big_dataset, subset=subset, coarsen_fn=subsample())
     
     # big_graph_name = "uk_big_ico"
     # small_graph_name = "uk_small_ico"
@@ -106,4 +122,4 @@ def test_subsample():
 
 if __name__ == "__main__":
     uk_hierarchy()
-    
+    # create_border_mask()